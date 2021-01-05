51485633aa6fd02607da831b32c1b8495f225adf
Liam <61555544+Liam-dev@users.noreply.github.com>
2021-01-05 15:41:09 +0000

    MonoGame.Forms and Content Pipeline setup complete

---
+++ Projectile Simulator/Projectile Simulator/DrawTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using MonoGame.Forms.Controls;
+
+namespace Projectile_Simulator
+{
+    class DrawTest : MonoGameControl
+    {
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw()
+        {
+            base.Draw();
+        }
+    }
+}
